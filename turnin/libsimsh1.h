4127eda6bb4c71fb7a6a0f83273a69f5bced3711
Shea <env.laser@gmail.com>
2017-02-25 23:15:27 -0700

    Add turnin files for part 1 of lab

---
+++ turnin/libsimsh1.h
@@ -0,0 +1,18 @@
+//
+// Created by Shea on 2017-02-25.
+//
+
+#ifndef LAB_2_LIBSIMSH_H
+#define LAB_2_LIBSIMSH_H
+#include <stdbool.h>
+#include "aux_files/chop_line.h"
+#include "aux_files/list.h"
+
+typedef enum validation_t { VALID = 0, BAD_AMPERSAND} validation_t;
+
+void print_prompt();
+validation_t validate_line(const chopped_line_t *line);
+const char* get_validation_result(validation_t result);
+bool launch_process(const chopped_line_t * line, bool wait); //true on success
+void setup_sigchld_handler();
+#endif //LAB_2_LIBSIMSH_H
---
+++ turnin/simsh1.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <errno.h>
+#include <wait.h>
+#include <string.h>
+#include "libsimsh1.h"
+
+
+int main()
+{
+    setup_sigchld_handler();
+    char* line = NULL;
+    size_t lineLength = 0;
+    ssize_t read;
+    print_prompt();
+    while((read = getline(&line, &lineLength, stdin)) != -1)
+    {
+        if (line[read - 1] == '\n') line[read -1] = '\0';
+        chopped_line_t * chopped_line = get_chopped_line(line);
+        if (chopped_line->num_tokens < 1)
+        {
+            free_chopped_line(chopped_line);
+            print_prompt();
+            continue;
+        }
+        else if (strcmp(chopped_line->tokens[0], "exit") == 0)
+        {
+            free_chopped_line(chopped_line);
+            errno = 0;
+            break;
+        }
+        validation_t validation_result = validate_line(chopped_line);
+        if (validation_result != VALID)
+        {
+            printf("Error: %s\n", get_validation_result(validation_result));
+        }
+        else
+        {
+            bool foreground = strcmp(chopped_line->tokens[chopped_line->num_tokens - 1], "&") != 0;
+            if (!launch_process(chopped_line, foreground))
+            {
+                perror("Error");
+            }
+        }
+        free_chopped_line(chopped_line);
+        print_prompt();
+    }
+    free(line);
+    if (!feof(stdin) && errno != 0)
+    {
+        perror("getline: ");
+        exit(EXIT_FAILURE);
+    }
+    wait(NULL);
+    return EXIT_SUCCESS;
+}
